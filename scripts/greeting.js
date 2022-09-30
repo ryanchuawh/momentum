6168b8ac7ab8f08a3ce9a07e3b8d11d88c14893e
Michelle Santiago <mdcsantiago0@gmail.com>
2022-09-30 13:31:11 +0800

    feat: Added greeting

---
+++ scripts/greeting.js
@@ -0,0 +1,24 @@
+const greetContainer = document.querySelector(".greet-container"), 
+    greetOfDay = greetContainer.querySelector("span");
+
+function getGreeting(){
+    const date = new Date();
+    const hour = date.getHours();
+    const greet =["Good Morning,","Good Afternoon,","Good Evening,"];
+    let greetText = '';
+    if (hour>=5&&hour<=12) {//5AM-12
+        greetText = greet[0];
+    }
+    else if (hour>12&&hour<18) { //1PM-6PM
+        greetText = greet[1];
+    }
+    else{
+        greetText = greet[2];//7PM-4AM
+    }
+    greetOfDay.innerText=greetText;
+}
+function init() {
+    getGreeting();
+    setInterval(getGreeting, 3600000);
+}
+init();
